8787ff56d5a9c10ac0115d56f10200e04775f251
Samnotwise <116502480+sam8314@users.noreply.github.com>
2026-01-10 22:33:28 +0100

    logic algo

---
+++ config.js
@@ -0,0 +1,8 @@
+let wordsAppList = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew'];
+let sentencesAppList = [
+    'The quick brown fox jumps over the lazy dog.',
+    'A journey of a thousand miles begins with a single step.',
+    'To be or not to be, that is the question.',
+    'All that glitters is not gold.',
+    'The pen is mightier than the sword.'
+];
--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("Fast-Typer script loaded.");
+let score = 0;
+
+// user chooses word or sentence mode
+let mode;
+while (mode !== 'word' && mode !== 'sentence') {
+    mode = prompt('Choose mode: word or sentence');
+}
+
+switch(mode) {
+    case 'word':
+        wordsMode();
+        break;
+    case 'sentence':
+        sentencesMode();
+        break;
+    default:
+        console.log('Invalid mode selected. Please refresh and try again.');
+        break;
+}
+
+function wordsMode(){
+    for (let i = 0; i < wordsAppList.length; i++) {
+        let wordUser = prompt('Type this word: ' + wordsAppList[i]);
+        if (wordUser === wordsAppList[i]) {
+            score++;
+        }
+        else{
+            score--;
+        }
+    }
+    returnScore(wordsAppList.length, score);
+}
+
+function sentencesMode(){
+    for (let i = 0; i < sentencesAppList.length; i++) {
+        let sentenceUser = prompt('Type this sentence: ' + sentencesAppList[i]);
+        if (sentenceUser === sentencesAppList[i]) {
+            score++;
+        }
+        else{
+            score--;
+        }
+    }
+    returnScore(sentencesAppList.length, score);
+}   
+
+function returnScore(nbQuestions, score){
+    return 'Your final score is: ' + score + '/' + nbQuestions;
+}
