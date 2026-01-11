77ebef96c560b5ff9783ed370f958915e8652c11
Samnotwise <116502480+sam8314@users.noreply.github.com>
2026-01-11 21:54:02 +0100

    re ordered functions

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-let score = 0;
+function chooseMode(){
+    let mode;
+    while (mode !== 'word' && mode !== 'sentence') {
+        mode = prompt('Choose mode: word or sentence');
+    }
 
-// user chooses word or sentence mode
-let mode;
-while (mode !== 'word' && mode !== 'sentence') {
-    mode = prompt('Choose mode: word or sentence');
+    return mode;
 }
 
-switch(mode) {
-    case 'word':
-        wordsMode();
-        break;
-    case 'sentence':
-        sentencesMode();
-        break;
-    default:
-        console.log('Invalid mode selected. Please refresh and try again.');
-        break;
-}
-
-function wordsMode(){
+function wordsMode(score){
     for (let i = 0; i < wordsAppList.length; i++) {
         let wordUser = prompt('Type this word: ' + wordsAppList[i]);
         if (wordUser === wordsAppList[i]) {
@@ -28,10 +17,9 @@ function wordsMode(){
             score--;
         }
     }
-    returnScore(wordsAppList.length, score);
 }
 
-function sentencesMode(){
+function sentencesMode(score){
     for (let i = 0; i < sentencesAppList.length; i++) {
         let sentenceUser = prompt('Type this sentence: ' + sentencesAppList[i]);
         if (sentenceUser === sentencesAppList[i]) {
@@ -41,9 +29,30 @@ function sentencesMode(){
             score--;
         }
     }
-    returnScore(sentencesAppList.length, score);
 }   
 
 function returnScore(nbQuestions, score){
     return 'Your final score is: ' + score + '/' + nbQuestions;
+}
+
+function main(){
+    let modeChosen = chooseMode();
+    console.log("modeChosen:", modeChosen);
+
+    let score = 0;
+
+    switch(modeChosen) {
+    case 'word':
+        wordsMode(score);
+        console.log(returnScore(wordsAppList.length, score));
+        break;
+    case 'sentence':
+        sentencesMode(score);
+        console.log(returnScore(sentencesAppList.length, score));
+        break;
+    default:
+        console.log('Invalid mode selected. Please refresh and try again.');
+        break;
+}
+
 }
