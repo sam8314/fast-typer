6a742e57075ed78f145df0c75cde0dbbef78959a
Samnotwise <116502480+sam8314@users.noreply.github.com>
2026-01-18 00:35:44 +0100

    finished logic

--- scripts/script.js
+++ scripts/script.js
@@ -9,11 +9,79 @@ function afficherResultat(score, nbMotsProposes) {
     spanScore.innerText = affichageScore
 }
 
+/**
+ * 
+ * @param {string} proposition : mot ou phrase qui va s'afficher dans la zone proposition 
+ */
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition");
+    zoneProposition.innerText = proposition;
+}
+
+/**
+ * 
+ * @returns {string} : le mode de jeu mots ou phrases
+ */
+function getModeJeu() {
+    if(document.getElementById("mots").checked) {
+        return "mots";
+    } else if(document.getElementById("phrases").checked) {
+        return "phrases";
+    }
+}
 
 function lancerJeu() {
     let score = 0
     let nbMotsProposes = 0
+    let i = 0;
+
+    let btnModeMots = document.getElementById("mots")
+    btnModeMots.addEventListener("click", function() {
+        console.log("mode mots sélectionné");
+        afficherProposition(wordsAppList[i]);
+    });
+
+    let btnModePhrases = document.getElementById("phrases")
+    btnModePhrases.addEventListener("click", function() {
+        console.log("mode phrases sélectionné");
+        afficherProposition(sentencesAppList[i]);
+    });
+
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    btnValiderMot.addEventListener("click", function() {
+        console.log("user typed valider");
+        i++;
+        console.log('i=', i);
+        console.log('mot à taper =', wordsAppList[i]);
+        console.log('phrase à taper =', sentencesAppList[i]); 
+
+        let mode = getModeJeu();
+        console.log('mode de jeu =', mode);
+
+        let target = (mode === "mots"? wordsAppList[i-1] : sentencesAppList[i-1]);
+        if (target===undefined) {
+            console.log("Fin du jeu");
+            afficherProposition("Fin du jeu");
+        }
+
+        if (mode === "mots") {
+            afficherProposition(wordsAppList[i]);
+        } else if (mode === "phrases") {
+            afficherProposition(sentencesAppList[i]);
+        }
+        
+        nbMotsProposes++;
+
+        let motSaisi = document.getElementById("inputEcriture").value;
+
+        if (motSaisi === (target)) {
+            score++;
+            console.log("Score augmenté ! Nouveau score :", score);
+        } else {
+            console.log("Mot incorrect. Score inchangé :", score);
+        }
+        afficherResultat(score, nbMotsProposes)
+    });
 
     
-    afficherResultat(score, nbMotsProposes)
 }
